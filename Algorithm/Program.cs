8372a922c00a228638d127e9b53c793f17e48745
Qingjie Bi (Beyondsoft) <you@example.com>
2018-07-19 17:43:57 +0800

    -madd several testFile

--- Algorithm/Program.cs
+++ Algorithm/Program.cs
@@ -16,11 +16,35 @@ namespace AlgorithmDemo
             //bfs.TestBFS();
             //StringAdd sta = new StringAdd();
             //sta.TestStringAdd();
-            TwoPointers tp = new TwoPointers();
+            //TwoPointers tp = new TwoPointers();
             //tp.TestTwoSumByHash();
-            tp.TestThreeSum();
+            //tp.TestThreeSum();
             //tp.TestThreeSumClosest();
             //tp.TestFourSum();
+
+            RuleTagConvert c = new RuleTagConvert();
+            //c.ConstructMethod("", "","");
+            //c.ConstructCatagory("", "");
+
+            #region
+            TableParametersTest test = new TableParametersTest();
+            int partnerID = 108;
+
+            String APPID = "XIEFfW27dN3C7Vqhfa";
+            List<FeedStatistic> list = new List<FeedStatistic>()
+            {
+                new FeedStatistic ()
+                {
+                    partnerId =108,
+                    AppID="XIEFfW27dN3C7Vqhfa",
+                   feedId ="MSRECOMMEND_CONTENT_2276010",
+                  ArticleKey = "108_XIEFfW27dN3C7Vqhfa_-5100120489162261699",
+                }
+            };
+
+            DateTime now = DateTime.Now;
+            test.FeedStatisticListUpdateInsert(partnerID, APPID, list, now);
+            #endregion
 
         }
     }
