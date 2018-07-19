f8dbc96687c48ac3ddfb11cdefc1e5095fac55de
Qingjie Bi (Beyondsoft) <you@example.com>
2018-07-19 18:29:40 +0800

    add LogMainDemo

---
+++ LogMailDemo/LineChart.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Drawing;
+using System.IO;
+using System.Windows.Forms.DataVisualization.Charting;
+
+namespace LogMailDemo
+{
+    public class LineChart
+    {
+        Chart ch;
+        public LineChart(DataLineChart DataLineChart)
+        {
+            ch = new Chart();
+            #region ChartArea
+            ChartArea ca = new ChartArea();
+
+            ca.Area3DStyle.Inclination = 40;
+            ca.Area3DStyle.IsClustered = true;
+            ca.Area3DStyle.IsRightAngleAxes = false;
+            ca.Area3DStyle.LightStyle = LightStyle.Realistic;
+            ca.Area3DStyle.Perspective = 9;
+            ca.Area3DStyle.Rotation = 25;
+            ca.Area3DStyle.WallWidth = 3;
+            //ca.AxisX.LabelStyle.Font = new System.Drawing.Font("Trebuchet MS", 8.25F, System.Drawing.FontStyle.Bold);
+            ca.AxisX.LineColor = Color.FromArgb(64, 64, 64, 64);
+            ca.AxisX.MajorGrid.LineColor = Color.FromArgb(64, 64, 64, 64);
+            // ca.AxisY.LabelStyle.Font = new Font("Trebuchet MS", 8.25F, System.Drawing.FontStyle.Bold);
+            ca.AxisY.LineColor = Color.FromArgb(64, 64, 64, 64);
+            ca.AxisY.MajorGrid.LineColor = Color.FromArgb(64, 64, 64, 64);
+            ca.BackColor = Color.OldLace;
+            ca.BackGradientStyle = GradientStyle.TopBottom;
+            ca.BackSecondaryColor = Color.White;
+            ca.BorderColor = Color.FromArgb(64, 64, 64, 64);
+            ca.BorderDashStyle = ChartDashStyle.Solid;
+            ca.ShadowColor = Color.Transparent;
+
+
+            //custom
+            ca.AxisX.LabelStyle.Format = "yyyyMMdd";
+            ca.AxisX.Title = "Date-Timeline";
+            ca.AxisX.TitleAlignment = StringAlignment.Center;
+            ca.AxisX.MajorGrid.Enabled = false;
+            ca.AxisX.Interval = 1.0;
+
+            ca.AxisX.LabelAutoFitStyle = LabelAutoFitStyles.None;
+            ca.AxisX.LabelStyle.Font = new Font("Calibri", 15, FontStyle.Regular);
+            ca.AxisX.IsMarginVisible = true;
+            ca.AxisX.TitleFont = new Font("Calibri", 25, FontStyle.Bold);
+
+            ca.AxisY.Title = "Data-In (records)";
+            ca.AxisY.LabelAutoFitStyle = LabelAutoFitStyles.None;
+            ca.AxisY.LabelStyle.Font = new Font("Calibri", 20, FontStyle.Regular);
+            ca.AxisY.TitleFont = new Font("Calibri", 25, FontStyle.Bold);
+
+            ch.BorderlineWidth = 14;
+            ch.Height = 768;
+            ch.Width = 1366;
+
+            ch.ChartAreas.Add(ca);
+            #endregion
+
+            #region Serises
+            foreach (var seriseData in DataLineChart.Data)
+            {
+                Series s = new Series() { ChartType = SeriesChartType.Line, IsValueShownAsLabel = true, Font = new Font("Calibri", 20, FontStyle.Regular), BorderWidth = 3, MarkerSize = 8, MarkerStyle = MarkerStyle.Circle, XValueType = ChartValueType.Date };
+                foreach (var dataPoint in seriseData)
+                {
+                    s.Points.Add(dataPoint);
+                }
+                ch.Series.Add(s);
+            }
+            #endregion
+            ch.Titles.Add(new Title(DataLineChart.Title, Docking.Top, new Font("Calibri", 35, FontStyle.Bold), Color.Black));
+        }
+
+        public void SaveChart(MemoryStream imageStream)
+        {
+
+            ch.SaveImage(imageStream, ChartImageFormat.Jpeg);
+            ch.SaveImage(DateTime.Now.Millisecond.ToString() + ".jpg", ChartImageFormat.Jpeg);
+        }
+
+        public MemoryStream SaveChart()
+        {
+            MemoryStream imageStream = new MemoryStream();
+            ch.SaveImage(imageStream, ChartImageFormat.Jpeg);
+            imageStream.Position = 0;
+            return imageStream;
+        }
+    }
+}
---
+++ LogMailDemo/Program.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms.DataVisualization.Charting;
+
+namespace LogMailDemo
+{
+    class Program
+    {
+        private static readonly log4net.ILog log = log4net.LogManager.GetLogger(MethodBase.GetCurrentMethod().DeclaringType);
+        static void Main(string[] args)
+        {
+
+            string assemblyFilePath = Assembly.GetExecutingAssembly().Location;
+            string assemblyDirPath = Path.GetDirectoryName(assemblyFilePath);
+            log4net.Config.XmlConfigurator.Configure(new FileInfo(Path.Combine(assemblyDirPath, "log4PDV.config")));
+            int timeout = string.IsNullOrEmpty(ConfigurationManager.AppSettings.Get("ConnectionTimeout")) ? 90 : int.Parse(ConfigurationManager.AppSettings.Get("ConnectionTimeout"));
+            try
+            {
+                List<List<FeedDailyCount>> PartnerDataCollection = new List<List<FeedDailyCount>>();
+                List<KeyValuePair<string, string>> PartnerIDNames = new List<KeyValuePair<string, string>>();
+                    //new DBHelper().GetPartnerIDName();
+                DateTime recordBeforeTime = new DateTime(Utils.BJNow.Year, Utils.BJNow.Month, Utils.BJNow.Day, Utils.BJNow.Hour, 0, 0);
+                List<string> ingnoreList = ConfigurationManager.AppSettings.Get("IgnoreAppIDs").Split(';').ToList();
+                foreach (var kv in PartnerIDNames)
+                {
+                    if (ingnoreList.Contains(kv.Key))
+                    {
+                        continue;
+                    }
+                    log.InfoFormat("start at :{0}, {1}", Utils.BJNow.ToString(Utils.DateTimeFormat), string.Format("PartnerID:{0},PartnerName:{1}", kv.Key, kv.Value));
+                    try
+                    {
+                        //PartnerDataCollection.Add(WareHouseDBManager.Instance.FeedSelectDailyCount(kv.Key, recordBeforeTime, 10, timeout));
+                    }
+                    catch (Exception e)
+                    {
+                        log.ErrorFormat("exception:{0}", e.Message);
+                        log.Error(e.StackTrace);
+                        continue;
+                    }
+                    log.InfoFormat("end at :{0}, {1}", Utils.BJNow.ToString(Utils.DateTimeFormat), string.Format("PartnerID:{0},PartnerName:{1}", kv.Key, kv.Value));
+                }
+
+                List<List<FeedDailyCount>> AbnormalPartnerDataCollection = new List<List<FeedDailyCount>>(PartnerDataCollection);
+               
+
+                List<DataLineChart> ldlc = new List<DataLineChart>();
+                foreach (var pd in PartnerDataCollection)
+                {
+                    ldlc.Add(ListFeedDailyCountToDataLineChart(pd, PartnerIDNames));
+                }
+                foreach (var dlc in ldlc)
+                {
+                    dlc.ImageStream = new LineChart(dlc).SaveChart();
+                    dlc.ImageStream.Position = 0;
+                }
+                SendOverall so = new SendOverall();
+                so.Send(ldlc, recordBeforeTime);
+
+                
+            }
+            catch (Exception e)
+            {
+                log.ErrorFormat("exception:{0}", e.Message);
+                log.Error(e.StackTrace);
+            }
+        }
+
+
+        public static DataLineChart ListFeedDailyCountToDataLineChart(List<FeedDailyCount> lfdc, List<KeyValuePair<string, string>> PartnerIDNames)
+        {
+            DataLineChart dlc = new DataLineChart();
+            List<DataPoint> ldp = new List<DataPoint>();
+            dlc.AppID = lfdc[0].AppID;
+            dlc.Title = string.IsNullOrWhiteSpace(PartnerIDNames.FirstOrDefault(m => m.Key.Equals(dlc.AppID)).Value) ? dlc.AppID : PartnerIDNames.FirstOrDefault(m => m.Key.Equals(dlc.AppID)).Value;//后续需要替换成partnerName,如果存在partnerName的话
+            foreach (var fdc in lfdc)
+            {
+                ldp.Add(new DataPoint(fdc.Date.ToOADate(), fdc.Count));
+            }
+            dlc.Data.Add(ldp);
+            return dlc;
+        }
+
+        
+    }
+}
