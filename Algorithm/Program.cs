bad65d42d37fcef3259e3eaed648a7ac5ce9e784
Qingjie Bi (Beyondsoft Corporation) <you@example.com>
2018-07-30 15:24:55 +0800

    add excel helper

--- Algorithm/Program.cs
+++ Algorithm/Program.cs
@@ -10,40 +10,51 @@ namespace AlgorithmDemo
     {
         static void Main(string[] args)
         {
+            try
+            {
+                #region RuleTag转换
+                //RuleTagConvert convert = new AlgorithmDemo.RuleTagConvert();
+                //convert.ConstructMethod ("");
+                #endregion
+                #region 构造Command命令
+                //ConstructGitAddCommand commnd = new AlgorithmDemo.ConstructGitAddCommand();
+                //commnd.TestConstructGitAddCommand();
+                #endregion
+                #region 测试表参数
+                //TableParametersTest test = new TableParametersTest();
+                //int partnerID = 108;
 
-            #region RuleTag转换
-            //RuleTagConvert convert = new AlgorithmDemo.RuleTagConvert();
-            //convert.ConstructMethod ("");
-            #endregion
-            #region 构造Command命令
-            //ConstructGitAddCommand commnd = new AlgorithmDemo.ConstructGitAddCommand();
-            //commnd.TestConstructGitAddCommand();
-            #endregion
-            #region 测试表参数
-            //TableParametersTest test = new TableParametersTest();
-            //int partnerID = 108;
+                //String APPID = "XIEFfW27dN3C7Vqhfa";
+                //List<FeedStatistic> list = new List<FeedStatistic>()
+                //{
+                //    new FeedStatistic ()
+                //    {
+                //        partnerId =108,
+                //        AppID="XIEFfW27dN3C7Vqhfa",
+                //       feedId ="MSRECOMMEND_CONTENT_2276010",
+                //      ArticleKey = "108_XIEFfW27dN3C7Vqhfa_-5100120489162261699",
+                //    }
+                //};
 
-            //String APPID = "XIEFfW27dN3C7Vqhfa";
-            //List<FeedStatistic> list = new List<FeedStatistic>()
-            //{
-            //    new FeedStatistic ()
-            //    {
-            //        partnerId =108,
-            //        AppID="XIEFfW27dN3C7Vqhfa",
-            //       feedId ="MSRECOMMEND_CONTENT_2276010",
-            //      ArticleKey = "108_XIEFfW27dN3C7Vqhfa_-5100120489162261699",
-            //    }
-            //};
+                //DateTime now = DateTime.Now;
+                //test.FeedStatisticListUpdateInsert(partnerID, APPID, list, now);
+                #endregion
+                #region 动态规划
+                //DynamicProgramming dp = new DynamicProgramming("ABCBDAB", "BDCABA");
+                //dp.PrintLCSTable();
+                //dp.PrintLCSSet();
 
-            //DateTime now = DateTime.Now;
-            //test.FeedStatisticListUpdateInsert(partnerID, APPID, list, now);
-            #endregion
-            #region 动态规划
-            DynamicProgramming dp = new DynamicProgramming("ABCBDAB", "BDCABA");
-            dp.PrintLCSTable();
-            dp.PrintLCSSet();
+                #endregion
 
-            #endregion
+                #region ExcelHelper
+                ExcelHelper helper = new AlgorithmDemo.ExcelHelper();
+                helper.ZLNameList();
+                #endregion
+            }
+            catch (Exception ex)
+            {
+                
+            }
 
         }
     }
--- LogMailDemo/Program.cs
+++ LogMailDemo/Program.cs
@@ -21,23 +21,72 @@ namespace LogMailDemo
             string assemblyDirPath = Path.GetDirectoryName(assemblyFilePath);
             log4net.Config.XmlConfigurator.Configure(new FileInfo(Path.Combine(assemblyDirPath, "log4net.config")));
             int timeout = string.IsNullOrEmpty(ConfigurationManager.AppSettings.Get("ConnectionTimeout")) ? 90 : int.Parse(ConfigurationManager.AppSettings.Get("ConnectionTimeout"));
-            try
+            while (true)
             {
+                try
+                {
 
-                throw new Exception("Hello World!");
-            }
-            catch (Exception e)
-            {
-                log.ErrorFormat("exception:{0}", e.Message);
-                log.Error(e.StackTrace);
-                var appender = log4net.LogManager.GetRepository().GetAppenders().First(P => P.Name.Equals("RollingLogFileAppender")) as RollingFileAppender;
-                String filePath = appender.File;
-                appender.Close();
-                SendOverall so = new SendOverall();
-                so.Send(filePath, e.Message, e.StackTrace, "ChunCheng");
+                    var appender = log4net.LogManager.GetRepository().GetAppenders().First(P => P.Name.Equals("RollingLogFileAppender")) as RollingFileAppender;
+                    String filePath = appender.File;
+                    System.Collections.IEnumerator t = log.Logger.Repository.ConfigurationMessages.GetEnumerator();
+                    while (t.MoveNext())
+                    {
+                        string name = t.Current.ToString();
+                        int startIndex = name.IndexOf(':');
+                        int endIndex = name.IndexOf(':', startIndex + 1);
+                        string tt = name.Substring(startIndex + 1, endIndex - (startIndex + 1));
+                        if (tt.Equals("FileAppender"))
+                        {
+                            startIndex = name.IndexOf('[');
+                            endIndex = name.IndexOf(']', startIndex + 1);
+                            filePath = name.Substring(startIndex + 1, endIndex - (startIndex + 1));
+                            break;
+                        }
+
+
+                    }
+                }
+                catch (Exception e)
+                {
+                    log.ErrorFormat("exception:{0}", e.Message);
+                    log.Error(e.StackTrace);
+
+                    var appender = log4net.LogManager.GetRepository().GetAppenders().First(P => P.Name.Equals("RollingLogFileAppender")) as RollingFileAppender;
+                    String filePath = appender.File;
+
+                    //appender.Close();//关闭后下次循环不能获取文件名
+
+                    // SendOverall so = new SendOverall();
+                    // so.Send(filePath, e.Message, e.StackTrace, "ChunCheng");
+
+                }
             }
         }
-       
+        private static void TestFileIsOpen(string path)
+        {
+
+            //FileStream fs = new FileStream(path, FileMode.Open , FileAccess.Read);
+
+            //BinaryReader br = new BinaryReader(fs);
+
+            //br.Read();
+
+            //Console.WriteLine("文件被打开");
+            //1是打开，0是关闭
+            int result = FileStatus.FileIsOpen(path);
+
+            Console.WriteLine(result);
+
+            //br.Close();
+
+            //Console.WriteLine("文件被关闭");
+
+            //result = FileStatus.FileIsOpen(path);
+
+            //Console.WriteLine(result);
+
+        }
+
 
 
 
